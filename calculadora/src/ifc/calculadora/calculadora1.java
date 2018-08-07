1e5ca2db48e56d67dc0a75444807b31ec3826dcd
Julia1234567891011 <juliagbressan@hotmail.com>
2018-08-07 08:45:53 -0300

    adicionado tela de usuario

--- calculadora/src/ifc/calculadora/calculadora1.java
+++ calculadora/src/ifc/calculadora/calculadora1.java
@@ -1,6 +1,28 @@
 package ifc.calculadora;
 
-public class calculadora1 {
+import javax.swing.JOptionPane;
 
+public class calculadora1 {
+ 
+	public static void main(String[] args) {
+		String numA = JOptionPane.showInputDialog(null, "digite o primeiro valor");
+		String numB = JOptionPane.showInputDialog(null,"digite o segundo valor");
+		
+		double a =Double.parseDouble(numA);
+		double b =Double.parseDouble(numB);
+		
+		//calcular
+		
+		int soma = (int) (a + b);
+		int sub = (int) (a - b);
+		int mult = (int) (a * b);
+		int div = (int) (a / b);
+		
+		//mostrar op 
+		
+		JOptionPane.showMessageDialog(null, "soma "+ soma + "\n"+
+		"Subtração"+ sub + "\n" + "Multiplicação" + mult + "\n" + "Divisão" + div);
+		
+	}
 	
 }
